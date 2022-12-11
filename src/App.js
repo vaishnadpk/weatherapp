a08f0c70f7d5c9e7fd589c02f86c71d93a84d61c
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-11 22:05:21 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This is codes by Vaishna Upendran and is an{" "}
-        <a
-          href="https://github.com/vaishnadpk/weatherapp"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced code.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This is codes by Vaishna Upendran and is an{" "}
+          <a
+            href="https://github.com/vaishnadpk/weatherapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced code.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
