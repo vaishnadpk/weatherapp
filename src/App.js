fc6581c2ea5694e78b7f6fb1f11406211bf9fdcc
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-11 19:14:26 +0100

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This is codes by Vaishna Upendran and is an{" "}
+        <a
+          href="https://github.com/vaishnadpk/weatherapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code.
+        </a>
+      </footer>
+    </div>
+  );
 }
