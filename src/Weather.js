e032c66adcfda970259c37590b839c9e6b4c9339
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-12 13:39:47 +0100

    made basic structure of app

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,46 @@
 import React from "react";
+import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
-  return <div>Hello world</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>Helsingborg</h1>
+      <ul>
+        <li>Sunday 22:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="weather description"
+          />
+          <span className="temp">-7°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 96%</li>
+            <li>Wind:2m/s</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Importing the Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 
