a4372353e72cd069c8b667131b40be8e6507ceb0
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-16 20:28:00 +0100

    updated object

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Helsingborg" />
         <footer>
           This is codes by Vaishna Upendran and is an{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -64,8 +64,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "2f554689e2d5b3aa846a49469f08491e";
-    let city = "Helsingborg";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
