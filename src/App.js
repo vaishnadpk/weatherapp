19ffbd91d54c034bbb5f1a67db8a2cc1c5aa2705
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-21 22:52:27 +0100

    changed title

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import Background from "./Background";
 import Weather from "./Weather";
 
 export default function App() {
+  const current = new Date();
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = months[current.getMonth()];
+  const date = `${current.getDate()} ${month},${current.getFullYear()}`;
   return (
     <div className="App">
       <Background />
       <div className="container">
-        <h1 className="title">Weather Forecast</h1>
+        <h1 className="title">
+          <span> Weather Now </span>
+          <span className="currentDate"> 🗓 {date} </span>
+        </h1>
         <Weather defaultCity="Helsingborg" />
         <footer>
           This is coded by Vaishna Upendran and is an Open-sourced code on{" "}
